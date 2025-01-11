8c57fc300daaa9d5da0bedc13d009ffd95008c73
Marco Mastrodonato <m.mastrodonato@gmail.com>
2025-01-11 16:45:06 +0100

    Updated ai_message_spec

--- spec/models/ai_message_spec.rb
+++ spec/models/ai_message_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe AiMessage, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:prompt) }
-    it { is_expected.to validate_presence_of(:answer) }
   end
 
   describe 'associations' do
